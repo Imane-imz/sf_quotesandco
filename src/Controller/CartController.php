07a677746c0c7b92104c54297b126a428f40b9e9
Imane-imz <imanemardhy@gmail.com>
2025-03-06 13:39:38 +0100

    Ajout au panier

---
+++ src/Controller/CartController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CartController extends AbstractController
+{
+    #[Route('/cart', name: 'app_cart')]
+    public function index(SessionInterface $session, ProductRepository $productRepository): Response
+    {
+        $cart = $session->get('cart', []); // Récupère le panier (ou un tableau vide s'il n'existe pas)
+        $cartData = [];
+
+        // Récupérer les détails des produits à partir de leur ID
+        foreach ($cart as $id => $quantity) {
+            $product = $productRepository->find($id);
+            if ($product) {
+                $cartData[] = [
+                    'product' => $product,
+                    'quantity' => $quantity
+                ];
+            }
+        }
+
+        return $this->render('cart/index.html.twig', [
+            'cart' => $cartData
+        ]);
+    }
+
+    #[Route('/cart/add/{id}', name: 'app_cart_add')]
+    public function add($id, SessionInterface $session): Response
+    {
+        $cart = $session->get('cart', []);
+
+        if (!empty($cart[$id])) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
+
+        $session->set('cart', $cart);
+
+        return $this->redirectToRoute('app_cart');
+    }
+
+    #[Route('/cart/remove/{id}', name: 'app_cart_remove')]
+    public function remove($id, SessionInterface $session): Response
+    {
+        $cart = $session->get('cart', []);
+
+        if (!empty($cart[$id])) {
+            unset($cart[$id]);
+        }
+
+        $session->set('cart', $cart);
+
+        return $this->redirectToRoute('app_cart');
+    }
+
+    #[Route('/cart/decrease/{id}', name: 'app_cart_decrease')]
+    public function decrease($id, SessionInterface $session): Response
+    {
+        $cart = $session->get('cart', []);
+
+        if (!empty($cart[$id])) {
+            if ($cart[$id] > 1) {
+                $cart[$id]--;
+            } else {
+                unset($cart[$id]); // Supprime l'article s'il est à 0
+            }
+        }
+
+        $session->set('cart', $cart);
+
+        return $this->redirectToRoute('app_cart');
+    }
+
+    #[Route('/cart/clear', name: 'app_cart_clear')]
+    public function clear(SessionInterface $session): Response
+    {
+        $session->remove('cart'); // Supprime complètement le panier
+
+        return $this->redirectToRoute('app_cart');
+    }
+}
